4f67d4ba386f416c818ab60ce55b0524fd320d44
Max Robertson <maxturboultimate@hotmail.com>
2019-07-18 10:17:36 -0700

    changed filenames slightly

--- src/main.cpp
+++ src/main.cpp
@@ -63,64 +63,64 @@ class PlayerUnit : public Aspen::Object::Object
   PlayerUnit(Object *parent = nullptr, std::string name = "PlayerUnit")
   {
   hoverAnim = new Aspen::Graphics::Animation(
-    new Aspen::Graphics::UniformSpritesheet("./resources/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
+    new Aspen::Graphics::UniformSpritesheet("./resources/Sprites/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
     AddChild(hoverAnim);
   hoverDest = new Aspen::Graphics::Animation(
-    new Aspen::Graphics::UniformSpritesheet("./resources/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
+    new Aspen::Graphics::UniformSpritesheet("./resources/Sprites/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
     AddChild(hoverDest);
   skidAnim = new Aspen::Graphics::Animation(
-    new Aspen::Graphics::UniformSpritesheet("./resources/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
+    new Aspen::Graphics::UniformSpritesheet("./resources/Sprites/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
     AddChild(skidAnim);
   skidsecAnim = new Aspen::Graphics::Animation(
-    new Aspen::Graphics::UniformSpritesheet("./resources/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
+    new Aspen::Graphics::UniformSpritesheet("./resources/Sprites/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
     AddChild(skidsecAnim);
   skidDest = new Aspen::Graphics::Animation(
-    new Aspen::Graphics::UniformSpritesheet("./resources/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
+    new Aspen::Graphics::UniformSpritesheet("./resources/Sprites/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
     AddChild(skidDest);
   artilleryAnim = new Aspen::Graphics::Animation(
-    new Aspen::Graphics::UniformSpritesheet("./resources/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
+    new Aspen::Graphics::UniformSpritesheet("./resources/Sprites/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
     AddChild(artilleryAnim);
   artyDest = new Aspen::Graphics::Animation(
-    new Aspen::Graphics::UniformSpritesheet("./resources/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
+    new Aspen::Graphics::UniformSpritesheet("./resources/Sprites/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
     AddChild(artyDest);
   hyperAnim = new Aspen::Graphics::Animation(
-    new Aspen::Graphics::UniformSpritesheet("./resources/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
+    new Aspen::Graphics::UniformSpritesheet("./resources/Sprites/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
     AddChild(hyperAnim);
   hyperAnimS = new Aspen::Graphics::Animation(
-    new Aspen::Graphics::UniformSpritesheet("./resources/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
+    new Aspen::Graphics::UniformSpritesheet("./resources/Sprites/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
     AddChild(hyperAnimS);
   hyperAnimM = new Aspen::Graphics::Animation(
-    new Aspen::Graphics::UniformSpritesheet("./resources/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
+    new Aspen::Graphics::UniformSpritesheet("./resources/Sprites/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
     AddChild(hyperAnimM);
   hyperAnimE = new Aspen::Graphics::Animation(
-    new Aspen::Graphics::UniformSpritesheet("./resources/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
+    new Aspen::Graphics::UniformSpritesheet("./resources/Sprites/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
     AddChild(hyperAnimE);
   hyperDest = new Aspen::Graphics::Animation(
-    new Aspen::Graphics::UniformSpritesheet("./resources/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
+    new Aspen::Graphics::UniformSpritesheet("./resources/Sprites/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
     AddChild(hyperDest);
   heavyAnim = new Aspen::Graphics::Animation(
-    new Aspen::Graphics::UniformSpritesheet("./resources/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
+    new Aspen::Graphics::UniformSpritesheet("./resources/Sprites/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
     AddChild(heavyAnim);
   heavyDest = new Aspen::Graphics::Animation(
-    new Aspen::Graphics::UniformSpritesheet("./resources/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
+    new Aspen::Graphics::UniformSpritesheet("./resources/Sprites/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
     AddChild(heavyDest);
   mechAnim = new Aspen::Graphics::Animation(
-    new Aspen::Graphics::UniformSpritesheet("./resources/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
+    new Aspen::Graphics::UniformSpritesheet("./resources/Sprites/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
     AddChild(mechAnim);
   mechDest = new Aspen::Graphics::Animation(
-    new Aspen::Graphics::UniformSpritesheet("./resources/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
+    new Aspen::Graphics::UniformSpritesheet("./resources/Sprites/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
     AddChild(mechDest);
   legWalkDiag = new Aspen::Graphics::Animation(
-    new Aspen::Graphics::UniformSpritesheet("./resources/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
+    new Aspen::Graphics::UniformSpritesheet("./resources/Sprites/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
     AddChild(legWalkDiag);
   legWalkDiag2 = new Aspen::Graphics::Animation(
-    new Aspen::Graphics::UniformSpritesheet("./resources/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
+    new Aspen::Graphics::UniformSpritesheet("./resources/Sprites/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
     AddChild(legWalkDiag2);
   legWalkHoriz = new Aspen::Graphics::Animation(
-    new Aspen::Graphics::UniformSpritesheet("./resources/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
+    new Aspen::Graphics::UniformSpritesheet("./resources/Sprites/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
     AddChild(legWalkHoriz);
   legWalkVert = new Aspen::Graphics::Animation(
-    new Aspen::Graphics::UniformSpritesheet("./resources/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
+    new Aspen::Graphics::UniformSpritesheet("./resources/Sprites/Firing Cycle.png", 32, 32, 8, nullptr, "FiringCycle"), 1.0f / 12.0f, this, "Firing Cycle");
     AddChild(legWalkVert);
 
   skidAnim->Deactivate();
@@ -164,6 +164,7 @@ class PlayerUnit : public Aspen::Object::Object
    //   }
    //  moveable->GetRigidbody()->SetCartesianVelocity(xv, yv);
    // }
+
 };
 class EnemyUnit : Aspen::Graphics::Animation
 {
