e4c08c94303b8f6b002573be84d385c80e127cc3
Max Robertson <maxturboultimate@hotmail.com>
2019-07-18 12:07:56 -0700

    commented out the error stuff

--- src/main.cpp
+++ src/main.cpp
@@ -211,10 +211,10 @@ class PlayerUnit : public Aspen::Object::Object
 */
 int main(int argc, char **argv)
 {
-    int resource = 155;
-    void OnUpdate(){
-        main.resource++;
-    };
+    //int resource = 155;
+    //void OnUpdate(){
+    //    main.resource++;
+    //};
     Aspen::Engine::Engine engine(Aspen::Engine::START_FLAGS::ALL);
 
     engine.FindChildOfType<Aspen::GameState::GameStateManager>()->LoadState<MyState>(true);
